aa2d98f484453117eace98c0a92f34cf575ba745
daishi <daishi@axlight.com>
2022-10-16 11:02:02 +0900

    fix example 02

--- examples/02_suspense/src/App.tsx
+++ examples/02_suspense/src/App.tsx
@@ -2,7 +2,7 @@
 
 import React, { Suspense, experimental_use as use, useTransition } from 'react';
 
-import { createStore } from 'zustand';
+import createStore from 'zustand/vanilla';
 import { useZustand } from 'use-zustand';
 
 const postStore = createStore<{
